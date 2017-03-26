89c33886f661f90b7e8a366844d3d6b1eca74008
Grader <labcontext@gmail.com>
2017-03-26 12:23:37 +0500

    fixed minor bugs

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 import View.Scanner;
 import View.Writer;
-import controller.CheckNumberController;
-import controller.GenNumberController;
 import model.Ugadai;
 
 import java.io.IOException;
@@ -12,16 +10,11 @@ public class Main {
         Scanner input = new Scanner();
         do {
             Writer writer = new Writer();
-            GenNumberController g = new GenNumberController();
-            CheckNumberController c = new CheckNumberController();
-            int attemptCount = writer.getAttemptCount();
-            int target = g.generate(writer.getMin(), writer.getMax());
 
             Ugadai ugadai = new Ugadai.UgadaiBuilder()
                     .setMin(writer.getMin())
                     .setMax(writer.getMax())
-                    .setAttemptCount(attemptCount)
-                    .setTarget(target).createUgadai();
+                    .createUgadai();
                     writer.check(input, ugadai);
         } while ("y".equals(input.ask("You want to play again? y/n")));
     }
